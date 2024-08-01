1637b88d284e80002e03203671f78b102eb89d1a
0xyicheng <ct359819@gmail.com>
2024-08-01 21:41:43 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-// this manifest is used temporarily for development purposes
+
 const manifestUrl = 'https://0xyicheng.github.io//my-twa1/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
