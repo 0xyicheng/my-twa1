6f27d6f42703c7432eaa9bd5b81a86ee47cafee6
0xyicheng <ct359819@gmail.com>
2024-08-01 21:36:18 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://0xyicheng.github.io/tg-frontend-demo/my-twa1/tonconnect-manifest.json';
+const manifestUrl = 'https://0xyicheng.github.io//my-twa1/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
